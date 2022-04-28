dfb3b46b3e6948c33714bb7de29701a0ce66285e
Nicolas Giot <nicolas@Nicolass-MacBook-Pro-2.local>
2022-04-28 18:45:53 -0500

    added new string prints

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
 # Main code for the program
 def main():
     print ("Hello World")
+    print ("changing code")
+    print ("new code")
+    print ("new code")
+    print ("new code")
+    print ("new code")
 
 
 main() #accesses main function
